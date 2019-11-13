31b8c3d27029eb6373f0d1f37dc6db2423d08124
Qingyun Lin <lin.qin@husky.neu.edu>
2019-11-13 15:09:22 -0800

    contact us design

--- src/ContactUs/ContactUs.jsx
+++ src/ContactUs/ContactUs.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
 import styles from './ContactUs.module.css';
-
+import Header from "../Header/Header";
 
 const ContactUs = () => {
   return (
 
-
               <div className="Contact-us">
-                  <div className="container">
-                      {/* To make form work, use your own formspree credentials in action="" */}
-                      <form action="https://formspree.io/youremail@domain.com" method="POST" name="contact">
+              <Header />
+                  <div className={styles.container}>
+
                           <div>
                               <label>Your Name: </label>
+                                <br></br>
                               <input type="text" name="name" />
                           </div>
                           <div>
                               <label>Your Email: </label>
+                                <br></br>
                               <input type="email" name="email" />
                           </div>
                           <div>
-                              <label>Message: </label>
+                              <message>Message: </message>
+                              <br></br>
                               <textarea name="message"></textarea>
                           </div>
                           <div>
                               <button type="submit">Send</button>
                           </div>
-                      </form>
+
                   </div>
               </div>
 
