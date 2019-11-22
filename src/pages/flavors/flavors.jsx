f7056dd91d5ab50a4257cba6599e3f3c86540ec6
Jianan Lin <jiananlin@Jianans-MacBook-Pro.local>
2019-11-21 16:39:15 -0800

    modified nav-bar

--- src/pages/flavors/flavors.jsx
+++ src/pages/flavors/flavors.jsx
@@ -4,6 +4,7 @@ import { List } from '../../ui/base/list/list';
 import { Page } from '../../ui/base/page-container/page-container';
 import { Cake } from './cake/cake';
 import styles from './flavors.module.css';
+import NavBar from '../../nav/nav-bar';
 const flavorsData = require('../../data/flavors.json');
 
 const flavorItems = flavorsData.map(f => ({
@@ -16,5 +17,6 @@ export const Flavors = memo(() => (
   <Page>
     <TitleLarge text="Cake"/>
     <List items={flavorItems} className={styles.list}/>
+    <NavBar />
   </Page>
 ));
