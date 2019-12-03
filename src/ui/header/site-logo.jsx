509ccda532bfb008575e21fdcb31ff83461accb9
xuanli-neu <li.xuan2@husky.neu.edu>
2019-12-03 12:46:08 -0800

    Update site-logo.jsx

--- src/ui/header/site-logo.jsx
+++ src/ui/header/site-logo.jsx
@@ -4,9 +4,11 @@ import styles from './site-logo.module.css';
 const LOGO_SRC = "/images/logo.png";
 
 export const SiteLogo = memo(() => (
+  <a href="http://localhost:3000/home">
   <img
     className={styles.logo}
     src={LOGO_SRC}
     alt="site-logo"
   />
+  </a>
 ));
