a291a6d1ff12c367098ebfb849d4544e2f251e39
Qingyun Lin <lin.qin@husky.neu.edu>
2019-12-04 11:01:37 -0800

    zipcode

--- src/ui/footer/newsletter-form/newsletter-form.jsx
+++ src/ui/footer/newsletter-form/newsletter-form.jsx
@@ -7,28 +7,31 @@ import styles from './newsletter-form.module.css';
 
 export const NewsLetterForm = () => {
   const [email, onEmailChange, isEmailError] = useNonEmptyTextinputState("");
+  const [zipcode, onZipChange, isZipError] = useNonEmptyTextinputState("");
   const [submitting, setSubmitting] = useState(false);
   const [startValidating, setStartValidating] = useState(false);
 
   const resetForm = useCallback(() => {
     setSubmitting(false);
     onEmailChange({ target: { value: '' }});
+    onZipChange({ target: { value: '' }});
     setStartValidating(false);
-  }, [setSubmitting, onEmailChange, setStartValidating]);
+  }, [setSubmitting, onEmailChange, setStartValidating,onZipChange]);
 
   const onSubmit = useCallback(async (e) => {
     e.preventDefault();
     setStartValidating(true);
-    if (isEmailError || submitting) {
+    if (isEmailError || submitting || isZipError) {
       return;
     };
     setSubmitting(true);
-    await NewsLetterServiceClient.subscribe(email)
+    await NewsLetterServiceClient.subscribe(email,zipcode)
     resetForm()
     alert('You have subscribed to our news letter');
-  }, [email, isEmailError, submitting, resetForm]);
+  }, [email, isEmailError, submitting, resetForm,isZipError ,zipcode]);
 
   return (
+
     <form onSubmit={onSubmit} className={styles.form}>
       <div className={styles.input}>
         <TextInput
@@ -39,13 +42,21 @@ export const NewsLetterForm = () => {
           onChange={onEmailChange}
           error={startValidating && isEmailError}
         />
+        <TextInput
+          type="text"
+          value={zipcode}
+          title="zipcode"
+          placeholder="zip"
+          onChange={onZipChange}
+          error={startValidating && isZipError}
+        />
       </div>
 
-      <div>
+       <div className={styles.button}>
         <SecondaryButton type="submit" disabled={submitting}>
           Sign Up
         </SecondaryButton>
       </div>
-    </form>
+  </form>
   )
 };
