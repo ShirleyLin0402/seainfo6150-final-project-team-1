77b27b90293bba4bfdc73851dd03b97faa425052
Jianan Lin <jiananlin@Jianans-MacBook-Pro.local>
2019-12-03 21:37:22 -0800

    adaption

--- src/pages/flavor/flavor.jsx
+++ src/pages/flavor/flavor.jsx
@@ -7,9 +7,11 @@ const flavorParams = require('../../data/flavors.json');
 
 export const FLavorPage = memo(({ title, description, image }) => (
   <Page>
-   <div className={styles.img}>
-      <img src={image} alt={title}/>
-  </div>
+    <div className={styles.box}>
+      <div className={styles.img}>
+        <img src={image} alt={title}/>
+      </div>
+    </div>
     <div className={styles.title}>
       <TitleMedium text={title}/>
     </div>
